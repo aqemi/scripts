69e4d8fcb9a1ddd4a301d26e5118e66a0c77d0b3
aqemi <god@universe>
2023-09-28 18:12:14 +0300

    more scripts

---
+++ keep-up-to-date.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+target_branch=${1?:"Usage: keep-up-to-date.sh <target branch> [base branch]"}
+base_branch=${2:-$(git rev-parse --abbrev-ref HEAD)}
+git checkout "$target_branch"
+git pull
+git rebase "$base_branch"
+git push --force
+git checkout -
